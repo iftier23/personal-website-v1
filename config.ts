f3b0fcedf4353379da3f9540f2c6af6cfb8c1bcc
Iftier Rahman <iftierrahman23@gmail.com>
2024-09-14 19:30:07 -0400

    Updated config.ts

--- config.ts
+++ config.ts
@@ -3,11 +3,11 @@
 import { Site } from "./types";
 
 export const SITE: Site = {
-    siteUrl: "https://www.bento.chrstnl.com/", // Always put "/" at the end of the URL
-    author: "chrstnl",
-    desc: "Welcome to Bent-o, A bento theme made for you!",
-    title: "Bent-o - NextJS Template",
-    ogImage: "https://www.chrstnl.com/oggraph-image.png",
+    siteUrl: "https://iftier.rahman.vercel.app/", // Always put "/" at the end of the URL
+    author: "Iftier Rahman",
+    desc: "Iftier Rahman's personal portfolio website",
+    title: "Iftier Rahman",
+    ogImage: "/og-image.png",
     keywords:
-        "NextJS, NextJS Templates, TailwindCSS, Premium NextJS and Tailwind CSS Templates, NextJS and TailwindCSS Templates, NextJS Boilerplate template, NextJS Boilerplate, Premium NextJS and TailwindCSS Templates, Premium NextJS and Tailwind CSS Themes, NextJS and Tailwind CSS Boilerplate, Premium NextJS and Tailwind CSS themes, nextjs themes, tailwind templates, NextJS themes, Personal portfolio, landing page, page template, developer portfolio, designer portfolio"
+        "Software Engineer, Software Developer, Software Engineering Intern, University of Toronto, Engineering, Personal portfolio, landing page, developer portfolio"
 };
